b8af966a55ebd843d21f3d35b1244d831eb127b4
Vinícius <viniciusmoraesdasilva25@gmail.com>
2024-11-03 10:40:03 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,15 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		
+		System.out.println("\n--- TEST 5: seller update---");
+		seller = sellerDao.findById(1);
+		seller.setName("Mikael Vargas");
+		seller.setEmail("mikaelvs@gmail.com");
+		sellerDao.update(seller);
+		
+		System.out.println("Update completed");
+		
 	}
 
 }
